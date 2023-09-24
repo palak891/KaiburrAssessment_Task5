163fff342ef0db5c071760101a90f22dfea81e39
palak891 <84228724+palak891@users.noreply.github.com>
2023-09-24 12:34:33 +0530

    Add files via upload

---
+++ Task5/ProjectKaibur/KaiburrApplication/src/main/java/com/kaiburr/ServerApplication.java
@@ -0,0 +1,22 @@
+package com.kaiburr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+//	echo "# KaiburrTask5-Ankit" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/ankit7277410093/KaiburrTask5-Ankit.git
+//	git push -u origin main
+
+}
---
+++ Task5/ProjectKaibur/KaiburrApplication/src/main/java/com/kaiburr/repository/ServerRepository.java
@@ -0,0 +1,16 @@
+package com.kaiburr.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.kaiburr.model.Server;
+
+@Repository
+public interface ServerRepository extends MongoRepository<Server, Long>{
+	@Query
+
+	public List<Server> findByName(String name);
+}
